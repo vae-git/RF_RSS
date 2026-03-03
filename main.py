a81355298ce079af625845e44545dfcde98b5dc3
vae-git <2533897950@qq.com>
2026-03-03 18:44:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,16 @@ def translate_text(text):
     except:
         return text # 翻译失败返回原文
 
-def send_pushdeer(content):
-    key = os.environ.get('PUSHDEER_KEY')
-    url = f"https://api2.pushdeer.com/message/push?pushkey={key}&text={content}&type=markdown"
-    requests.get(url)
-
+def send_wechat(content):
+    # 将 PUSHDEER_KEY 替换为你的 Server酱 SendKey
+    send_key = os.environ.get('SERVER_CHAN_KEY')
+    # Server酱的接口地址
+    url = f"https://sctapi.ftqq.com/{send_key}.send"
+    data = {
+        "title": "射频期刊每日更新",
+        "desp": content
+    }
+    requests.post(url, data=data)
 def main():
     combined_msg = "### 📡 射频期刊每日更新 \n\n"
     for url in RSS_FEEDS:
